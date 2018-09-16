0cc1d846ef2ac05b9c98cae3602ea709b04b49be
Balazs Strenner <bstrenner@gatech.edu>
2018-09-15 21:27:29 -0400

    Change Measure internals to array instead of dict

--- src/traintracks/measure.jl
+++ src/traintracks/measure.jl
@@ -1,60 +1,58 @@
-# export Measure, branchmeasure, zeromeasure
 
 
 module Measures
 
+export Measure, branchmeasure, zeromeasure
+
 using Donut.TrainTracks
 
-struct Measure{T<:Number}
-    # values::Array{T, 1}
-    values::Dict{Int, T}
+struct Measure{T}
+    values::Array{T, 1}
 
-    function Measure{T}(tt::TrainTrack, valuedict::Dict{Int, T}, allownegatives::Bool=false) where {T<:Number}
+    function Measure{T}(tt::TrainTrack, valuearray::Array{T, 1}, allownegatives::Bool=false) where {T}
+        if length(valuearray) != length(branches(tt))
+            error("The length of the values array should equal the number of branches")
+        end
         if !allownegatives
-            for value in values(valuedict)
+            for value in valuearray
                 if value < 0
                     error("The measure should be nonnegative.")
                 end
             end
         end
+        brancharray = branches(tt)
+        maxbranch_number = maximum(brancharray)
+        values = zeros(T, maxbranch_number)
+        for i in eachindex(brancharray)
+            br = brancharray[i]
+            values[br] = valuearray[i]
+        end
         for sw in switches(tt)
-            sums = [sum([valuedict[abs(br)] for br in outgoing_branches(tt, sign*sw)]) for sign in (-1, 1)]
+            sums = [sum([values[abs(br)] for br in outgoing_branches(tt, sign*sw)]) for sign in (-1, 1)]
             if sums[1] != sums[2]
                 error("The switch condition is not satisfied at switch $(sw).")
             end
         end
-        new(valuedict)
-    end 
-
-    function Measure{T}(tt::TrainTrack, valuearray::Array{T, 1}) where {T<:Number}
-        if length(valuearray) != length(branches(tt))
-            error("The length of the values array should equal the number of branches")
-        end
-        brancharray = branches(tt)
-        valuedict = Dict([(brancharray[i], valuearray[i]) for i in eachindex(valuearray)])
-        Measure{T}(tt, valuedict)
+        new(values)
     end 
 end
 
 
 
 function zeromeasure(tt::TrainTrack, type)
-    # maxbranch_number = maximum(branches(tt))
     Measure{type}(tt, zeros(type, length(branches(tt))))
 end
 
 
 function branchmeasure(measure::Measure, branchindex::Int)
-    try 
-        return measure.values[abs(branchindex)]
-    catch error
-        if isa(error, KeyError)
-            error("There is no branch with index $(branchindex)")
-        else
-            throw(error)
-        end
-    end
+    measure.values[abs(branchindex)]
 end
 
+"""
+Can mess up switch conditions.
+"""
+function _setmeasure(measure::Measure, branchindex::Int, newvalue)
+    measure.values[abs(branchindex)] = newvalue 
+end
 
 end
--- test/traintracks/measure_test.jl
+++ test/traintracks/measure_test.jl
@@ -2,23 +2,36 @@
 module MeasureTest
 
 using Test
-using Donut.TrainTracks: TrainTrack
-using Donut.TrainTracks.Measures: Measure, branchmeasure, zeromeasure
+using Donut.TrainTracks
+using Donut.TrainTracks.Measures
+import Donut
 
-tt = TrainTrack([[1, 2], [-1, -2]])
-measure = Measure{Int}(tt, [3, 8])
-@test branchmeasure(measure, 1) == 3
-@test branchmeasure(measure, -1) == 3
-@test branchmeasure(measure, 2) == 8
-@test branchmeasure(measure, -2) == 8
-@test typeof(branchmeasure(measure, -2)) == Int
 
-measure = Measure{BigInt}(tt, BigInt[3, 8])
-@test typeof(branchmeasure(measure, -2)) == BigInt
+@testset "Measures" begin
+    tt = TrainTrack([[1, 2], [-1, -2]])
+    measure = Measure{Int}(tt, [3, 8])
+    @test branchmeasure(measure, 1) == 3
+    @test branchmeasure(measure, -1) == 3
+    @test branchmeasure(measure, 2) == 8
+    @test branchmeasure(measure, -2) == 8
+    @test typeof(branchmeasure(measure, -2)) == Int
 
-measure = zeromeasure(tt, Int)
-@test branchmeasure(measure, 1) == 0
-@test branchmeasure(measure, -2) == 0
+    measure = Measure{BigInt}(tt, BigInt[3, 8])
+    @test typeof(branchmeasure(measure, -2)) == BigInt
 
+    measure = zeromeasure(tt, Int)
+    @test branchmeasure(measure, 1) == 0
+    @test branchmeasure(measure, -2) == 0
+
+    tt = TrainTrack([[1, 2], [-3], [3], [-1, -2]])
+    @test_throws ErrorException Measure{Int}(tt, [3, 1, 8])
+    @test_throws ErrorException Measure{Int}(tt, [3, 8])
+    @test_throws ErrorException Measure{Int}(tt, [-1, -1, -2])
+
+    tt = TrainTrack([[1, 2], [-3], [3], [-1, -2]])
+    measure = Measure{Int}(tt, [3, 5, 8])
+    Donut.TrainTracks.Measures._setmeasure(measure, 3, 100)
+    @test branchmeasure(measure, 3) == 100
+end
 
 end
